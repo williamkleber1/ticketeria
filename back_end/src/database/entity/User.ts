2dbfbccb9df03651142c215633ba67af3793360d
william kleber <wkas@ic.ufal.br>
2021-09-22 00:55:38 -0300

    Refactor User
    Atualização da classe User

--- back_end/src/database/entity/User.ts
+++ back_end/src/database/entity/User.ts
@@ -1,19 +1,57 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
-import { ITimeStamp } from "./TimeStamp";
-
-@Entity()
-export class User extends ITimeStamp {
-
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    Unique,
+    CreateDateColumn,
+    UpdateDateColumn,
+    BeforeInsert,
+    BeforeUpdate
+  } from "typeorm";
+  import { Length, IsNotEmpty } from "class-validator";
+  import * as bcrypt from "bcryptjs";
+  
+  @Entity()
+  @Unique(["username"])
+  export class User {
     @PrimaryGeneratedColumn()
     id: number;
+  
+    @Column()
+    @Length(4, 20)
+    username: string;
+  
+    @Column()
+    @Length(4, 100)
+    password: string;
+  
+    @Column()
+    @IsNotEmpty()
+    role: string;
 
     @Column()
-    firstName: string;
+    @IsNotEmpty()
+    name: string;
 
     @Column()
-    lastName: string;
-
+    @IsNotEmpty()
+    email: string;
+  
     @Column()
-    age: number;
+    @CreateDateColumn()
+    createdAt: Date;
+  
+    @Column()
+    @UpdateDateColumn()
+    updatedAt: Date;
 
-}
+    @BeforeInsert()
+    @BeforeUpdate()
+    hashPassword() {
+      this.password = bcrypt.hashSync(this.password, 8);
+    }
+  
+    checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+      return bcrypt.compareSync(unencryptedPassword, this.password);
+    }
+  }
